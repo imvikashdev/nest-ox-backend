772aae2955b7cc7e6a9ce04eadd67006cbb635be
Vikash Choudhary <imvikashhereyo@gmail.com>
2023-06-06 22:26:38 +0530

    initial socket setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,11 @@ import { PrismaService } from './prisma/prisma.service';
 import { GameService } from './game/game.service';
 import { PlayerService } from './player/player.service';
 import { MoveService } from './move/move.service';
+import { GameGateway } from './game/game.gateway';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService, PrismaService, GameService, PlayerService, MoveService],
+  providers: [AppService, PrismaService, GameService, PlayerService, MoveService, GameGateway],
 })
 export class AppModule {}
